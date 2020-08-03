dd6b6b17e09d2e9a781e979f4e5088eb80f4d017
Jameschip <>
2020-08-03 14:30:46 +0100

    Add a line mode

--- draw.cpp
+++ draw.cpp
@@ -1,10 +1,19 @@
 #include <ncurses.h>
 
+void add_colour( int c ) {
+    attron( COLOR_PAIR( c ) );  
+    addch(' ');
+    attron( COLOR_PAIR( c ) );
+}
+
 int main( int argc, char *argv[] ) {
     
     int row,col;
     int c_row, c_col = 0;
 	int ch;
+    bool line = false;
+    int last_colour = 8;
+    
 	initscr();
 	cbreak();
     noecho();
@@ -20,45 +29,64 @@ int main( int argc, char *argv[] ) {
 	refresh();
 
     start_color();
-    init_pair(1, COLOR_BLACK, COLOR_WHITE);    
-    init_pair(2, COLOR_WHITE, COLOR_CYAN);
-    init_pair(3, COLOR_WHITE, COLOR_MAGENTA);
-    init_pair(4, COLOR_WHITE, COLOR_GREEN);    
-    init_pair(5, COLOR_WHITE, COLOR_YELLOW); 
-    init_pair(6, COLOR_WHITE, COLOR_BLUE); 
-    init_pair(7, COLOR_WHITE, COLOR_RED); 
-    init_pair(8, COLOR_WHITE, COLOR_BLACK); 
-    bkgd( COLOR_PAIR(1) );
+    init_pair(1, COLOR_WHITE, COLOR_BLACK); 
+    init_pair(2, COLOR_WHITE, COLOR_RED); 
+    init_pair(3, COLOR_WHITE, COLOR_GREEN);    
+    init_pair(4, COLOR_WHITE, COLOR_YELLOW); 
+    init_pair(5, COLOR_WHITE, COLOR_CYAN);
+    init_pair(6, COLOR_WHITE, COLOR_MAGENTA);
+    init_pair(7, COLOR_WHITE, COLOR_BLUE); 
+    init_pair(8, COLOR_BLACK, COLOR_WHITE);    
+    bkgd( COLOR_PAIR(8) );
 
     clear();    
 
 	while((ch = getch()) != KEY_F(1))
 	{	
+		getyx( stdscr, c_row, c_col );
         switch(ch)
 		{	
 
             case KEY_LEFT:
-				getyx( stdscr, c_row, c_col );
-                if ( c_col > 0)
-                    wmove( stdscr, c_row, c_col - 1 );
+                if ( c_col > 0) {
+                    if ( line ) {
+                        add_colour( last_colour );
+                        wmove( stdscr, c_row, c_col - 1 );
+                    } else {
+                        wmove( stdscr, c_row, c_col - 1 );
+                    }
+                }
 				break;
 
 			case KEY_RIGHT:
-				getyx( stdscr, c_row, c_col );
                 if ( c_col < col )
-                    wmove( stdscr, c_row, c_col + 1 );
+                    if ( line ) {
+                        add_colour( last_colour ); 
+                        wmove( stdscr, c_row, c_col + 1 );
+                    } else {
+                        wmove( stdscr, c_row, c_col + 1 );
+                    }
 				break;
 
 			case KEY_UP:
-				getyx( stdscr, c_row, c_col );
                 if ( c_row > 0)
-                    wmove( stdscr, c_row - 1, c_col );
+                    if ( line ) {
+                        add_colour( last_colour );
+                        wmove( stdscr, c_row - 1, c_col );    
+                    } else {
+                        wmove( stdscr, c_row - 1, c_col );
+                    }
                 break;
 
 			case KEY_DOWN:
-				getyx( stdscr, c_row, c_col );
-                if ( c_row < row)
-                    wmove( stdscr, c_row + 1, c_col );
+                if ( c_row < row ) {
+                    if ( line ) {
+                        add_colour( last_colour );
+                        wmove( stdscr, c_row + 1, c_col );     
+                    } else {
+                        wmove( stdscr, c_row + 1, c_col);
+                    }
+                }
 				break;
             
             case '1':
@@ -69,10 +97,13 @@ int main( int argc, char *argv[] ) {
             case '6':
             case '7':
             case '8':
-                attron( COLOR_PAIR( ch - 48 ) );
-                addch(' ');
-                attroff( COLOR_PAIR( ch - 48 ) );
+                last_colour = ch - 48;
+                add_colour( last_colour );
+                line = false;
                 break;
+
+            case 'l':
+                line = !line;            
 
             default:
                 break;
@@ -83,3 +114,4 @@ int main( int argc, char *argv[] ) {
     
 	return 0;
 }
+
